1f4ef581ced0350e6f68b0a41d50a1d4c30ba591
marincenusa <access1992>
2016-05-20 23:14:45 +0300

    edit app menu

--- app/src/main/java/ro/netex/upack/MapActivity.java
+++ app/src/main/java/ro/netex/upack/MapActivity.java
@@ -80,9 +80,7 @@ public class MapActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        }  else if (id == R.id.nav_send) {
 
         }
 
