a6fb5ad1445aba65a642084aefb8414048f0c533
Claudiu Patt <max@netex.ro>
2016-05-21 15:41:51 +0300

    fixed REST requests

--- app/src/main/java/ro/netex/upack/UPackApi.java
+++ app/src/main/java/ro/netex/upack/UPackApi.java
@@ -1,14 +1,21 @@
 package ro.netex.upack;
 import android.util.Log;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class UPackApi {
+
+    public static final String TAG = UPackApi.class.getSimpleName();
+
     private String serverAddress;
     private MapActivity map;
 
@@ -18,10 +25,40 @@ public class UPackApi {
     }
 
     public void call(String route) {
-        request("http://" + serverAddress + "/rest/" + route + ".php");
+        try {
+            request("http://" + serverAddress + "/rest/" + route + ".php");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void request(String url) {
+    public void request(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+            client.newCall(request).enqueue(new Callback() {
+                @Override public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
+                }
+
+                @Override public void onResponse(Call call, Response response) throws IOException {
+                    JSONArray json = null;
+                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+                    String jsonResponse=response.body().string();
+                    try {
+                        json=new JSONArray(jsonResponse);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    Log.v(TAG,jsonResponse);
+                    map.populateMapWithSuppliers(json);
+                    System.out.println(response.body().string());
+                }
+            });
+        }
+
+   /* public void request(String url) {
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this.map);
         // Request a string response from the provided URL.
@@ -39,5 +76,6 @@ public class UPackApi {
         });
         // Add the request to the RequestQueue.
         queue.add(jsonObjReq);
-    }
+    } */
+
 }
