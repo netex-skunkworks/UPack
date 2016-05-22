edcee1702584d659f66fe5e92d12c56e85a90387
Claudiu Mihut <claudiu_mihut@netex.ro>
2016-05-22 07:58:17 +0300

    improvements..in the morning..

--- app/src/main/java/ro/netex/upack/PackageActivity.java
+++ app/src/main/java/ro/netex/upack/PackageActivity.java
@@ -8,13 +8,17 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PackageActivity extends FragmentActivity {
 
@@ -47,21 +51,28 @@ public class PackageActivity extends FragmentActivity {
     {
         ListView listView1 = (ListView) findViewById(R.id.packageList);
 
-        List<String> items = new ArrayList<String>();
+
+        ArrayList<Map<String, String>> itemsList = new ArrayList<Map<String, String>>();
+        String[] from = { "id", "name" };
+        int[] to = { android.R.id.text1, android.R.id.text2 };
+
         for (int i = 0; i < data.length(); i++) {
             JSONObject address = null;
             JSONObject delivery_description = null;
             try {
                 address              = data.getJSONObject(i).getJSONObject("customer").getJSONObject("address");
                 delivery_description = data.getJSONObject(i).getJSONObject("delivery_description");
+                int id               = data.getJSONObject(i).getInt("id");
+                itemsList.add(putData(String.valueOf(id), address.getString("street")+" - "+delivery_description.getString("distance")+" ("+delivery_description.getString("duration")+")"));
 
-                items.add(address.getString("street")+" - "+delivery_description.getString("distance")+" ("+delivery_description.getString("duration")+")");
+
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, items);
+
+        SimpleAdapter adapter = new SimpleAdapter(this, itemsList,
+                android.R.layout.simple_list_item_2, from, to);
         listView1.setAdapter(adapter);
 
         listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -73,12 +84,22 @@ public class PackageActivity extends FragmentActivity {
 
         listView1.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int pos, long id) {
-                UPackApi.activity = this;
-                AppController appController = new AppController(this);
-                appController.updateStatus("accepted");
+            public boolean onItemLongClick(AdapterView<?> arg0, View view, int pos, long id) {
+                TextView v = (TextView)view.findViewById(R.id.text1);
+                String itemId = v.getText().toString();
+                Log.d("idul:", itemId);
+//                AppController appController = new AppController(this);
+//                appController.updateStatus("accepted", String.valueOf(pos));
                 return true;
             }
         });
     }
+
+    private HashMap<String, String> putData(String id, String name) {
+        HashMap<String, String> item = new HashMap<String, String>();
+        item.put("id", id);
+        item.put("name", name);
+        return item;
+    }
+
 }
